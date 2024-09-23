eb7bf7ece24bed6765a89e96da92ff00131d80ec
mark15963 <83621809+mark15963@users.noreply.github.com>
2024-09-24 01:22:40 +0300

    Add files via upload

---
+++ contact.js
@@ -0,0 +1,92 @@
+const form = document.querySelector("form");
+const fullName = document.getElementById("name");
+const email = document.getElementById("email");
+const phone = document.getElementById("phone");
+const subject = document.getElementById("subject");
+const mess = document.getElementById("message");
+
+
+function sendEmail(){
+    const bodyMessage = "Имя: " + fullName.value + "<br> Почта: " + email.value + "<br> Номер телефона: " + phone.value + "<br> Сообщение: "+ mess.value;
+
+    Email.send({
+        SecureToken : "ee58a901-1e7f-4af4-a9fb-94252632ef9e",
+        To : "20wildpony02@gmail.com",
+        From : "mark15963@gmail.com",
+        Subject : subject.value,
+        Body : bodyMessage
+    }).then(
+      message => {
+        if (message == "OK"){
+            Swal.fire({
+                title: "Отлично!",
+                text: "Сообщение отправлено!",
+                icon: "success"
+              });
+        }
+      }
+    );
+}
+
+function checkInputs(){
+    const items = document.querySelectorAll(".item");
+
+    for (const item of items) {
+        if(item.value == ""){
+            item.classList.add("error");
+            item.parentElement.classList.add("error");
+        }
+
+        if(items[1].value != ""){
+            checkEmail();
+        }
+
+        items[1].addEventListener("keyup", () => {
+            checkEmail();
+        });
+
+        item.addEventListener("keyup", () => {
+            if (item.value != ""){
+                item.classList.remove("error");
+                item.parentElement.classList.remove("error");
+            }
+            else{
+                item.classList.add("error");
+                item.parentElement.classList.add("error");
+            }
+        })
+    }
+}
+
+function checkEmail() {
+    const emailRegex = /^([a-z\d\.-]+)@([a-z\d-]+)\.([a-z]{2,3})(\.[a-z]{2,3})?$/;
+    const errorTxtEmail = document.querySelector(".error-txt.email");
+
+    if(!email.value.match(emailRegex)){
+        email.classList.add("error");
+        email.parentElement.classList.add("error");
+
+        if(email.value != ""){
+            errorTxtEmail.innerText = "Введите настоящий почтовый адрес";
+        }
+        else{
+            errorTxtEmail.innerText = "Поле не может быть пустым";
+        }
+    }
+    else{
+        email.classList.remove("error");
+        email.parentElement.classList.remove("error");
+    }
+}
+
+form.addEventListener("submit", (e) => {
+    e.preventDefault();
+    checkInputs();
+
+    if(!fullName.classList.contains("error") && !email.classList.contains("error") && !phone.classList.contains("error") && !subject.classList.contains("error") && !mess.classList.contains("error")){
+        sendEmail();
+
+        form.reset();
+        return false;
+    }
+});
---
+++ index.js
@@ -0,0 +1,8 @@
+function myFunc(){
+    document.getElementById("demo").innerHTML = "Hey";
+}
+
+function myFunc2(){
+    const testConst=69;
+    console.log(testConst);
+}
