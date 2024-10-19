323f68164ae0be683068de19675941a9a353f76b
mark15963 <83621809+mark15963@users.noreply.github.com>
2024-10-19 03:41:02 +0300

    Add files via upload

--- index.js
+++ index.js
@@ -6,3 +6,13 @@ function myFunc2(){
     const testConst=69;
     console.log(testConst);
 }
+
+var aTags = document.querySelectorAll('a[data-href]');
+
+for(var i = 0; i < aTags.length; i++){
+    var aTag = aTags[i];
+    aTag.addEventListener('click', function(e){
+        var ele = e.target;
+        window.location.replace(ele.getAttribute('data-href'));
+    });    
+}
